6d83950d28023b13a83588ed7565750d6a76ad22
Stormybeast <laveen1994vasnani@outlook.com>
2018-04-01 18:10:07 -0400

    Intrasec Second Commit

--- IntraSec-Sniffer.py
+++ IntraSec-Sniffer.py
@@ -2,9 +2,9 @@ import socket,sys
 from struct import *
 
 
-def eth_addr(a):
-    b= "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a[0],a[1],a[2],a[3],a[4],a[5])
-    return b
+def get_e_addr(a):
+    e_addr = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a[0],a[1],a[2],a[3],a[4],a[5])
+    return e_addr
 
 
 if __name__ == "__main__":
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
     while True:
         packet = s.recvfrom(65565)
-
+        print(packet)
         packet = packet[0]
-        eth_length = 14
+        e_length = 14
 
-        eth_header = packet[:eth_length]
-        eth = unpack('!6s6sH', eth_header)
-        eth_protocol = socket.ntohs(eth[2])
-        if eth_protocol == 8 and eth_addr(packet[0:6]) != "ff:ff:ff:ff:ff:ff":
-            print('Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol))
+        e_header = packet[:e_length]
+        e = unpack('!6s6sH', e_header)
+        e_protocol = socket.ntohs(e[2])
+        if e_protocol == 8 and get_e_addr(packet[0:6]) != "ff:ff:ff:ff:ff:ff":
+            print('Destination MAC : ' + get_e_addr(packet[0:6]) + ' Source MAC : ' + get_e_addr(packet[6:12]) + ' Protocol : ' + str(e_protocol))
