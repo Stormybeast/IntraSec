19809aa58493b96e0b60a556ad4c660cd7aa91d5
Stormybeast <laveen1994vasnani@outlook.com>
2018-04-01 18:22:45 -0400

    Third Commit

--- IntraSec-Sniffer.py
+++ IntraSec-Sniffer.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
 
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
     while True:
-        packet = s.recvfrom(65565)
-        print(packet)
-        packet = packet[0]
+        pack = s.recvfrom(65565)
+        print(pack)
+        pack = pack[0]
         e_length = 14
 
-        e_header = packet[:e_length]
+        e_header = pack[:e_length]
         e = unpack('!6s6sH', e_header)
         e_protocol = socket.ntohs(e[2])
-        if e_protocol == 8 and get_e_addr(packet[0:6]) != "ff:ff:ff:ff:ff:ff":
-            print('Destination MAC : ' + get_e_addr(packet[0:6]) + ' Source MAC : ' + get_e_addr(packet[6:12]) + ' Protocol : ' + str(e_protocol))
+        if e_protocol == 8 and get_e_addr(pack[0:6]) != "ff:ff:ff:ff:ff:ff":
+            print('Destination MAC : ' + get_e_addr(pack[0:6]) + ' Source MAC : ' + get_e_addr(pack[6:12]) + ' Protocol : ' + str(e_protocol))
