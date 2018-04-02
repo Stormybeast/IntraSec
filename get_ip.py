7481393734249587d66e5c662f38bf5544b51549
Stormybeast <laveen1994vasnani@outlook.com>
2018-04-01 23:21:56 -0400

    Some Changes

--- get_ip.py
+++ get_ip.py
@@ -1,18 +1,39 @@
 import os
-import re
+import time
 
-def get_ip():
-    ip = os.system("ifconfig wlp8s0 | grep \"inet addr\" | cut -d ':' -f 2 | cut -d ' ' -f 1")
+
+def get_sys_ip():
+    ip = os.popen("ifconfig wlp8s0 | grep \"inet addr\" | cut -d ':' -f 2 | cut -d ' ' -f 1")
+    ip = str(ip.read())
+    print("The system ip is: "+ip)
     return ip
 
 
-if __name__ == "__main__":
-    ip = get_ip()
-    print(ip)
+def get_ip_list():
     os.system("arp -i wlp8s0 -n | grep -oE \"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\" > /media/codemaster_sb94/0800FF7600FF6958/MyPlayground/\"Python Projects\"/IntraSec/ips.txt")
+    ips = open("/media/codemaster_sb94/0800FF7600FF6958/MyPlayground/Python Projects/IntraSec/ips.txt")
+    ip_list = ips.readlines()
+    ip_list = [x.strip() for x in ip_list]
+    return ip_list
 
-    arp = open("/media/codemaster_sb94/0800FF7600FF6958/MyPlayground/Python Projects/IntraSec/ips.txt")
-    arp_list = arp.readlines()
-    arp_list = [x.strip() for x in arp_list]
-    print(arp_list)
-    print(len(arp_list))
+
+def get_mac_list():
+    os.system("arp -i wlp8s0 -n | grep -oE \"([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\" > /media/codemaster_sb94/0800FF7600FF6958/MyPlayground/\"Python Projects\"/IntraSec/macs.txt")
+    macs = open("/media/codemaster_sb94/0800FF7600FF6958/MyPlayground/Python Projects/IntraSec/macs.txt")
+    mac_list = macs.readlines()
+    mac_list = [x.strip() for x in mac_list]
+    return mac_list
+
+
+if __name__ == "__main__":
+    sys_ip = get_sys_ip().strip()
+    print("fping -g "+sys_ip+"/24")
+    os.system("fping -g "+sys_ip+"/24")
+    time.sleep(10)
+    ip_list = get_ip_list()
+    mac_list = get_mac_list()
+    arp={}
+    for i in range(len(ip_list)):
+        arp[mac_list[i]] = ip_list[i]
+
+    print(arp)
